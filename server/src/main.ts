f599e7fef9ea5a82dc29f8b80dd175d652db83bb
Bagher hosseini <bagherhosseini03@gmail.com>
2023-12-20 16:12:25 +0100

    chore: changes for deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,16 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   dotenv.config();
-  const httpsOptions = {
-    key: fs.readFileSync(`${__dirname}/../src/localhost-key.pem`),
-    cert: fs.readFileSync(`${__dirname}/../src/localhost.pem`),
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const port = process.env.PORT || 8080;
+  // const httpsOptions = {
+  //   key: fs.readFileSync(`${__dirname}/../src/localhost-key.pem`),
+  //   cert: fs.readFileSync(`${__dirname}/../src/localhost.pem`),
+  // };
+  const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
     origin: [process.env.CLIENT_URL],
@@ -23,6 +24,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(3335);
+  await app.listen(port);
 }
 bootstrap();
