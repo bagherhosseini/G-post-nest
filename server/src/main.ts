ff2248a9eb53d9406b9f306b385d95741a2f2c92
Bagher hosseini <bagherhosseini03@gmail.com>
2023-12-21 20:33:35 +0100

    fix: setting cookie

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,18 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
 // import * as fs from 'fs';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  dotenv.config();
   const port = process.env.PORT || 8080;
   // const httpsOptions = {
   //   key: fs.readFileSync(`${__dirname}/../src/localhost-key.pem`),
   //   cert: fs.readFileSync(`${__dirname}/../src/localhost.pem`),
   // };
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  app.use(cookieParser(process.env.JWT_SECRET));
   app.enableCors({
     origin: [process.env.CLIENT_URL],
     credentials: true,
