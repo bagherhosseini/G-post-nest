f4af1ec61a5e4b6a585f887e2761540a9a1c4345
Bagher hosseini <bagherhosseini03@gmail.com>
2023-12-23 22:52:44 +0100

    chore: display authToken

--- client/src/features/app/components/authenticatedRoute/authenticatedRoute.jsx
+++ client/src/features/app/components/authenticatedRoute/authenticatedRoute.jsx
@@ -4,20 +4,24 @@ import Cookies from "js-cookie"
 export default function AuthenticatedRoute({ children }) {
     const authToken = Cookies.get("authToken");
     return (
-        authToken === "undefined" || authToken === undefined || authToken === null || authToken === "" ? (
-            <p style={{
-                position: 'absolute',
-                fontSize: '3rem',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                fontFamily: 'sans-serif',
-            }}>
-                Loading<span style={{ color: "white", marginLeft: "5px" }}>&#9679; &#9679; &#9679;</span>
-            </p>
-        ) : (
-            children
-        )
-
+        <>
+        <p>{authToken}</p>
+            {
+                authToken === "undefined" || authToken === undefined || authToken === null || authToken === "" ? (
+                    <p style={{
+                        position: 'absolute',
+                        fontSize: '3rem',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        fontFamily: 'sans-serif',
+                    }}>
+                        Loading<span style={{ color: "white", marginLeft: "5px" }}>&#9679; &#9679; &#9679;</span>
+                    </p>
+                ) : (
+                    children
+                )
+            }
+        </>
     )
 }
