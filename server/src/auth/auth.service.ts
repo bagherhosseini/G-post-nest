9c1764bbb73d8bde08d1dc0d661d92e3040a39ff
Bagher hosseini <bagherhosseini03@gmail.com>
2023-12-23 13:26:32 +0100

    fix: set cookie

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
       res.cookie('authToken', authToken, {
         maxAge: 3600000,
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: 'lax',
         secure: true,
       });
 
