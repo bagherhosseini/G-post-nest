65136dffcb8e1f1646cef320a35c72319065dd05
Bagher hosseini <Bagher.Hosseini@yh.nackademin.se>
2024-05-08 15:30:29 +0200

    feat(server): main local

---
+++ server/src/mainLocal.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const port = process.env.PORT || 8080;
+  const httpsOptions = {
+    key: fs.readFileSync(`${__dirname}/../src/localhost-key.pem`),
+    cert: fs.readFileSync(`${__dirname}/../src/localhost.pem`),
+  };
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
+  app.use(cookieParser(process.env.JWT_SECRET));
+  app.set('trust proxy', 1);
+  app.enableCors({
+    origin: [process.env.CLIENT_URL, 'https://localhost:3000'],
+    credentials: true,
+    exposedHeaders: ['Set-Cookie'],
+    methods: ['GET', 'POST'],
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Content-Type',
+      'Authorization',
+    ],
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(port);
+}
+bootstrap();
